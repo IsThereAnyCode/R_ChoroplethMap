ffe5b0cd8dda88ff9285af0cfd5fd6ef36c4a1ea
IsThereAnyCode <hjk7625@gmail.com>
2022-08-27 22:10:53 +0900

    ChoroplethMap to R

---
+++ USACrime_ChoroplethMap.R
@@ -0,0 +1,29 @@
+install.packages("mapproj")
+install.packages("ggiraphExtra")
+library(ggiraphExtra)
+str(USArrests)
+head(USArrests)
+
+library(tibble)
+crime <- rownames_to_column(USArrests, var = "state")
+crime$state <- tolower(crime$state)
+
+str(crime)
+
+install.packages("maps")
+library(ggplot2)
+states_map <- map_data("state")
+str(states_map)
+
+# 단계 구분도 만들기
+ggChoropleth(data = crime,            # 지도에 표현할 데이터
+             aes(fill = Murder,       # 색깔로 표현할 변수
+                 map_id = state),     # 지역 기준 변수
+             map = states_map)        # 지도 데이터
+
+# 인터랙티브 단계 구분도 만들기
+ggChoropleth(data = crime,            # 지도에 표현할 데이터
+             aes(fill = Murder,       # 색깔로 표현할 변수
+                 map_id = state),     # 지역 기준 변수
+             map = states_map,        # 지도 데이터
+             interactive = T)         # 인터랙티브
